ae9e2eb45e914968064501ab01a79fe3cfbee0b7
Jayson Gazeley <89006649+jgazeley@users.noreply.github.com>
2021-08-27 15:01:45 -0700

    Update honda.py

--- honda.py
+++ honda.py
@@ -4,137 +4,123 @@ import time
 UNLK = 16
 LOCK = 12
 TRUNK = 19
-DOME = 20
 HORN = 21
 PLIGHTS = 6
+START = 22
+DOME = 20
 ACC = 23
 IGN = 27
-START = 22
 BUTTON = 5
+
+# Index  0      1     2      3      4       5      6    7    8      9
+gpio = [UNLK, LOCK, TRUNK, HORN, PLIGHTS, START, DOME, ACC, IGN, BUTTON]
 
 io.setwarnings(False)
 io.setmode(io.BCM)
-io.setup(UNLK, io.OUT)
-io.setup(LOCK, io.OUT)
-io.setup(TRUNK, io.OUT)
-io.setup(DOME, io.OUT)
-io.setup(HORN, io.OUT)
-io.setup(PLIGHTS, io.OUT)
-io.setup(ACC, io.OUT)
-io.setup(IGN, io.OUT)
-io.setup(START, io.OUT)
-io.setup(BUTTON, io.IN, io.PUD_UP)
-io.output(UNLK, 1)
-io.output(LOCK, 1)
-io.output(TRUNK, 1)
-io.output(DOME, io.input(DOME))
-io.output(HORN, 1)
-io.output(PLIGHTS, 1)
-io.output(ACC, io.input(ACC))
-io.output(IGN, io.input(IGN))
-io.output(START, 1)
+
+for x in range(0, 10): # Set BUTTON as an input, all other channels as outputs
+  if x < 9:
+    io.setup(gpio[x], io.OUT)
+    if x < 6:
+      io.output(gpio[x], 1) # Set outputs UNLK, LOCK, TRUNK, HORN, PLIGHTS, START as HIGH
+    if x >= 6:
+      io.output(gpio[x], io.input(gpio[x])) # Set DOME, ACC, IGN according to input value
+  else:
+    io.setup(gpio[x], io.IN, io.PUD_UP)
+
 
 def activate(pin):
-    io.output(pin, 0)
-    time.sleep(.1)
-    io.output(pin, 1)
+  io.output(pin, 0)
+  time.sleep(.1)
+  io.output(pin, 1)
 
 def toggle(pin):
-    io.output(pin, not io.input(pin))
+  io.output(pin, not io.input(pin))
 
 def check(pin):
-    result = (io.input(pin))
-    return result
+  result = io.input(pin)
+  return result
 
 def full_check():
-    print("UNLK    (GPIO16): | " + str(io.input(16)) + " |")
-    print("LOCK    (GPIO12): | " + str(io.input(12)) + " |")
-    print("TRUNK   (GPIO19): | " + str(io.input(19)) + " |")
-    print("DOME    (GPIO20): | " + str(io.input(20)) + " |")
-    print("HORN    (GPIO21): | " + str(io.input(21)) + " |")
-    print("PLIGHTS (GPIO06): | " + str(io.input(6)) + " |")
-    print("ACC     (GPIO23): | " + str(io.input(23)) + " |")
-    print("IGN     (GPIO27): | " + str(io.input(27)) + " |")
-    print("START   (GPIO22): | " + str(io.input(22)) + " |")
-    print("BUTTON  (GPIO05): | " + str(io.input(5)) + " |")
+  for x in gpio:
+    print("(GPIO " + str(x) + ") = " + str(io.input(x)))
 
 def lock():
-    activate(LOCK)
-    with open("lockstat.txt", "r") as lockstat:
-        if '0' in lockstat.read():
-            io.output(PLIGHTS, 0)
-            time.sleep(.6)
-            io.output(PLIGHTS, 1)
-            print("Car Locked")
-            with open("lockstat.txt", "w") as lockstat:
-                lockstat.write("LOCKED = 1")
-        else:
-            print("Car Already Locked")
+  activate(LOCK)
+  with open("lockstat", "r") as lockstat:
+    if '0' in lockstat.read():
+      io.output(PLIGHTS, 0)
+      time.sleep(.6)
+      io.output(PLIGHTS, 1)
+      print("Car Locked")
+      with open("lockstat", "w") as lockstat:
+        lockstat.write("LOCKED = 1")
+    else:
+      print("Car Already Locked")
 
 def unlock():
-    activate(UNLK)
-    with open("lockstat.txt", "r") as lockstat:
-        if '1' in lockstat.read():
-            io.output(PLIGHTS, 0)
-            time.sleep(.6)
-            io.output(PLIGHTS, 1)
-            time.sleep(.7)
-            io.output(PLIGHTS, 0)
-            time.sleep(.7)
-            io.output(PLIGHTS, 1)
-            print("Car Unlocked")
-            with open("lockstat.txt", "w") as lockstat:
-                lockstat.write("LOCKED = 0")
-        else:
-            print("Car Already Unlocked")
+  activate(UNLK)
+  with open("lockstat", "r") as lockstat:
+    if '1' in lockstat.read():
+      io.output(PLIGHTS, 0)
+      time.sleep(.6)
+      io.output(PLIGHTS, 1)
+      time.sleep(.7)
+      io.output(PLIGHTS, 0)
+      time.sleep(.7)
+      io.output(PLIGHTS, 1)
+      print("Car Unlocked")
+      with open("lockstat", "w") as lockstat:
+        lockstat.write("LOCKED = 0")
+    else:
+      print("Car Already Unlocked")
 
 def trunk():
-    activate(TRUNK)
-    print("Trunk Release Activated.")
+  activate(TRUNK)
+  print("Trunk Release Activated.")
 
 def horn():
-    activate(HORN)
-    print("Horn Activated")
+  activate(HORN)
+  print("Horn Activated")
 
 def dome():
-    toggle(DOME)
-    if io.input(DOME) == 0:
-        print("Dome Light On.")
-    if io.input(DOME) == 1:
-        print("Dome Light Off.")
+  toggle(DOME)
+  if not check(DOME):
+    print("Dome Light On.")
+  if check(DOME):
+    print("Dome Light Off.")
 
 def acc():
-    toggle(ACC)
-    if io.input(ACC) == 0:
-        print("Accessory On.")
-    if io.input(ACC) == 1:
-        print("Accessory Off.")
+  toggle(ACC)
+  if not check(ACC):
+    print("Accessory On.")
+  if check(ACC):
+    print("Accessory Off.")
 
 def ign():
-    toggle(IGN)
-    if io.input(IGN) == 0:
-        print("Ignition On.")
-    if io.input(IGN) == 1:
-        print("Ignition Off.")
+  toggle(IGN)
+  if not check(IGN):
+    print("Ignition On.")
+  if check(IGN):
+    print("Ignition Off.")
 
 def car_on():
-    io.output(ACC, 0)
-    io.output(IGN, 0)
-    print("Accessory and Ignition On.")
+  io.output(ACC, 0)
+  io.output(IGN, 0)
+  print("Accessory and Ignition On.")
 
 def start():
-    io.output(IGN, 0)
-    io.output(ACC, 1)
-    time.sleep(1)
-    io.output(START, 0)
-    time.sleep(.8)
-    io.output(START, 1)
-    time.sleep(.3)
-    io.output(ACC, 0)
-    print("Car Started.")
+  io.output(IGN, 0)
+  io.output(ACC, 1)
+  time.sleep(1)
+  io.output(START, 0)
+  time.sleep(.8)
+  io.output(START, 1)
+  time.sleep(.3)
+  io.output(ACC, 0)
+  print("Car Started.")
 
 def kill():
-    io.output(DOME, 1)
-    io.output(ACC, 1)
-    io.output(IGN, 1)
-    print("All Systems Off.")
+  for x in range(0, 9):
+    io.output(gpio[x], 1)
+  print("All Systems Off.")
