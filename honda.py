8e9b52eeabf421368f038c41a4ed3513933e945f
Jayson Gazeley <89006649+jgazeley@users.noreply.github.com>
2021-08-27 01:52:02 -0700

    Update honda.py

--- honda.py
+++ honda.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as io
 import time
 
-# GPIO Assignments #
 UNLK = 16
 LOCK = 12
 TRUNK = 19
@@ -11,10 +10,10 @@ PLIGHTS = 6
 ACC = 23
 IGN = 27
 START = 22
+BUTTON = 5
 
 io.setwarnings(False)
 io.setmode(io.BCM)
-
 io.setup(UNLK, io.OUT)
 io.setup(LOCK, io.OUT)
 io.setup(TRUNK, io.OUT)
@@ -24,22 +23,43 @@ io.setup(PLIGHTS, io.OUT)
 io.setup(ACC, io.OUT)
 io.setup(IGN, io.OUT)
 io.setup(START, io.OUT)
-
+io.setup(BUTTON, io.IN, io.PUD_UP)
 io.output(UNLK, 1)
 io.output(LOCK, 1)
 io.output(TRUNK, 1)
-io.output(DOME, 1)
+io.output(DOME, io.input(DOME))
 io.output(HORN, 1)
 io.output(PLIGHTS, 1)
-io.output(ACC, 1)
-io.output(IGN, 1)
+io.output(ACC, io.input(ACC))
+io.output(IGN, io.input(IGN))
 io.output(START, 1)
 
-# Lock/Unlock #
-def lock():
-    io.output(LOCK, 0)
+def activate(pin):
+    io.output(pin, 0)
     time.sleep(.1)
-    io.output(LOCK, 1)
+    io.output(pin, 1)
+
+def toggle(pin):
+    io.output(pin, not io.input(pin))
+
+def check(pin):
+    result = (io.input(pin))
+    return result
+
+def full_check():
+    print("UNLK    (GPIO16): | " + str(io.input(16)) + " |")
+    print("LOCK    (GPIO12): | " + str(io.input(12)) + " |")
+    print("TRUNK   (GPIO19): | " + str(io.input(19)) + " |")
+    print("DOME    (GPIO20): | " + str(io.input(20)) + " |")
+    print("HORN    (GPIO21): | " + str(io.input(21)) + " |")
+    print("PLIGHTS (GPIO06): | " + str(io.input(6)) + " |")
+    print("ACC     (GPIO23): | " + str(io.input(23)) + " |")
+    print("IGN     (GPIO27): | " + str(io.input(27)) + " |")
+    print("START   (GPIO22): | " + str(io.input(22)) + " |")
+    print("BUTTON  (GPIO05): | " + str(io.input(5)) + " |")
+
+def lock():
+    activate(LOCK)
     with open("lockstat.txt", "r") as lockstat:
         if '0' in lockstat.read():
             io.output(PLIGHTS, 0)
@@ -52,9 +72,7 @@ def lock():
             print("Car Already Locked")
 
 def unlock():
-    io.output(UNLK, 0)
-    time.sleep(.1)
-    io.output(UNLK, 1)
+    activate(UNLK)
     with open("lockstat.txt", "r") as lockstat:
         if '1' in lockstat.read():
             io.output(PLIGHTS, 0)
@@ -70,45 +88,40 @@ def unlock():
         else:
             print("Car Already Unlocked")
 
-# # Trunk #
 def trunk():
-    io.output(TRUNK, 0)
-    time.sleep(.1)
-    io.output(TRUNK, 1)
+    activate(TRUNK)
     print("Trunk Release Activated.")
 
-# Horn #
 def horn():
-    io.output(HORN, 0)
-    time.sleep(.1)
-    io.output(HORN, 1)
+    activate(HORN)
     print("Horn Activated")
 
-# Dome Light (On/Off) #
 def dome():
-    io.output(DOME, not io.input(DOME))
+    toggle(DOME)
     if io.input(DOME) == 0:
         print("Dome Light On.")
     if io.input(DOME) == 1:
         print("Dome Light Off.")
 
-# # Accessory (On/Off) #
 def acc():
-    io.output(ACC, not io.input(ACC))
+    toggle(ACC)
     if io.input(ACC) == 0:
         print("Accessory On.")
     if io.input(ACC) == 1:
         print("Accessory Off.")
 
-# Ignition (On/Off) #
 def ign():
-    io.output(IGN, not io.input(IGN))
+    toggle(IGN)
     if io.input(IGN) == 0:
         print("Ignition On.")
     if io.input(IGN) == 1:
         print("Ignition Off.")
 
-# Start #
+def car_on():
+    io.output(ACC, 0)
+    io.output(IGN, 0)
+    print("Accessory and Ignition On.")
+
 def start():
     io.output(IGN, 0)
     io.output(ACC, 1)
